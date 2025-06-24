d438193e624f5713775d17b9e58bd47d4e579e39
kukla <kukla.1989@ukr.net>
2025-06-24 11:33:48 +0300

    add timer UI layout

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Main.scss';
+import TimeUnit from "../TimeUnit/TimeUnit.jsx";
 
 function Main() {
-  return (
-    <div className='main'>
-      <div className="title">ready for applying</div>
-      <div className="t-minus">t - minus:</div>
+  return (<div className='main'>
+    <div className="title">ready for applying</div>
+    <div className="t-minus">t - minus:</div>
+    <div className="timer">
+      <TimeUnit/>
+      <TimeUnit/>
+      <TimeUnit/>
+      <TimeUnit/>
     </div>
-  );
+  </div>);
 }
 
 export default Main;
---
+++ src/components/TimeUnit/TimeUnit.jsx
@@ -0,0 +1,8 @@
+import './TimeUnit.scss';
+
+export default function TimeUnit() {
+  return <div className='time-unit'>
+    <div className="time">24</div>
+    <div className="time-type">hours</div>
+  </div>;
+}
