df78ea4260d18b4963669792529efe76c7eee1a2
kukla <kukla.1989@ukr.net>
2025-06-25 15:48:50 +0300

    add timer functionality

---
+++ src/_uttils/_types.ts
@@ -0,0 +1,7 @@
+export type TimeRemaining = {
+  total: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
 import './Main.scss';
 import TimeUnit from "../TimeUnit/TimeUnit";
+import {TimeRemaining} from "../../_uttils/_types";
+
+const targetDate = new Date('2025-07-09T00:00:00')
 
 function Main() {
+  const [timeLeft, setTimeLeft] = React.useState<TimeRemaining>(getTimeRemaining(targetDate));
+  useEffect(() => {
+    const intervalID: number = setInterval(() => {
+      setTimeLeft(_ => {
+        const remaining = getTimeRemaining(targetDate);
+        if (remaining.total < 0) {
+          clearInterval(intervalID);
+          return {total: 0, days: 0, hours: 0, minutes: 0, seconds: 0};
+        }
+
+        return remaining;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalID);
+  }, [])
+
   return (<div className='main'>
     <div className="title">ready for applying</div>
+
     <div className="t-minus">t - minus:</div>
+
     <div className="timer">
       <TimeUnit/>
       <TimeUnit/>
@@ -16,3 +39,13 @@ function Main() {
 }
 
 export default Main;
+
+function getTimeRemaining(targetDate: Date): TimeRemaining {
+  const total = targetDate.getTime() - new Date().getTime();
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+  const days = Math.floor(total / 1000 / 60 / 60 / 24);
+
+  return {total, days, hours, minutes, seconds};
+}
