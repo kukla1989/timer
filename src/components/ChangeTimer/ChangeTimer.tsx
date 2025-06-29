ca3d4c0d431337d3fa682017e1bd48c13a5f5454
kukla <kukla.1989@ukr.net>
2025-06-29 14:12:03 +0300

    add error handling for changeTimer

--- src/components/ChangeTimer/ChangeTimer.tsx
+++ src/components/ChangeTimer/ChangeTimer.tsx
@@ -9,21 +9,32 @@ function ChangeTimer({changeTimerInfo}: ChangeTimerProps) {
   const [showOverlay, setShowOverlay] = useState<boolean>(false);
   const [title, setTitle] = useState<string>('');
   const [days, setDays] = useState<string>('');
+  const [error, setError] = useState<string>('');
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    if (isNaN(parseInt(days)) || parseInt(days) <= 0) {
+      return setError('days must be a positive number')
+    }
+
+    if (title.length === 0) {
+      return setError('title must be not empty')
+    }
+
     setShowOverlay(false);
     changeTimerInfo(parseInt(days), title)
     setDays('');
     setTitle('');
+    setError('');
   };
 
   return (
     <div className='wrapper'>
-      <div className='change-timer' onClick={() => setShowOverlay(true)}>
+      <div className='change-timer-btn' onClick={() => setShowOverlay(true)}>
         change time
       </div>
 
       {showOverlay && <div className="overlay">
+        <div className="error">{error}</div>
         <input className="title" type="text" placeholder="new title"
                value={title}
                onChange={(e) => setTitle(e.target.value)}/>
